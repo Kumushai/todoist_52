7879cf733326f6b74a6e24f477abd5e4ee8ad6ff
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-06-30 15:52:00 +0600

    Создала включаемый шаблон для формы задачи, html файлы для удаления и обновления, обновила файл для создания, обновила urls.py

--- todoist/webapp/forms.py
+++ todoist/webapp/forms.py
@@ -9,6 +9,7 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["content", "status", "details", "created_at"]
-        widgets = {"content": widgets.Textarea(attrs={"cols": 30, "rows": 7})}
+        widgets = {"details": widgets.Textarea(attrs={"cols": 30, "rows": 7}),
+                   "created_at": widgets.DateInput(attrs={"type": "date"})}
         error_messages = {"title": {"required": "Поле обязательное"}}
 
--- todoist/webapp/urls.py
+++ todoist/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import todo_list_view, todo_create_view, todo_view, delete_todo_view
+from webapp.views import todo_list_view, todo_create_view, todo_view, delete_todo_view, todo_update_view
 
 urlpatterns = [
     path('', todo_list_view, name='index'),
     path('todos/add/', todo_create_view, name='todo_add'),
     path('todo/<int:pk>', todo_view, name='todo_view'),
     path('todos/delete/<int:pk>', delete_todo_view, name='delete_todo'),
+    path('todos/update/<int:pk>', todo_update_view, name='update_todo'),
 ]
