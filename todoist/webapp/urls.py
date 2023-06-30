7a709c9295243bb563cbe3a674443e81c3f36bc8
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-06-30 17:09:14 +0600

    Внесла изменения в html документы, исправила ошибку с удалением

--- todoist/webapp/urls.py
+++ todoist/webapp/urls.py
@@ -5,7 +5,7 @@ from webapp.views import todo_list_view, todo_create_view, todo_view, delete_tod
 urlpatterns = [
     path('', todo_list_view, name='index'),
     path('todos/add/', todo_create_view, name='todo_add'),
-    path('todo/<int:pk>', todo_view, name='todo_view'),
-    path('todos/delete/<int:pk>', delete_todo_view, name='delete_todo'),
-    path('todos/update/<int:pk>', todo_update_view, name='update_todo'),
+    path('todo/<int:pk>/', todo_view, name='todo_view'),
+    path('todos/delete/<int:pk>/', delete_todo_view, name='delete_todo'),
+    path('todos/update/<int:pk>/', todo_update_view, name='update_todo'),
 ]
